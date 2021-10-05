a53d313c9b0c774bcb8b91fedf60724d01e880a3
daniel103 <90566213+daniel103@users.noreply.github.com>
2021-10-05 23:14:42 +0300

    Add files via upload

---
+++ homework responsive/scripts.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const navBar = document.querySelector(".nav-bar");
+
+menuIcon.addEventListener ('click', () => {
+    navBar.classList.toggle("change");
+});
